73c3201a18273637fba8b467352470fb19caeb8b
ahmed atef <ahmedelhabak101@gmail.com>
2025-08-24 12:58:12 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/ibm-coursera-react/",
-    plugins: [react()],
+  // base: "/ibm-coursera-react/",  
+  plugins: [react()],
+  server: {
+    host: true, 
+    port: 6001,  
+    open: true,
+  },
 })
